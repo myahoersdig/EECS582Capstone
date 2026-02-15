7aaedfe7531e734e6c72bb9ee5a776f87ab4e895
Mya <mhoersdig@gmail.com>
2026-02-14 22:26:48 -0600

    includes a splashscreen that displays before the main app activity

---
+++ app/src/main/java/com/example/eecs582capstone/Entry.java
@@ -0,0 +1,14 @@
+package com.example.eecs582capstone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Entry extends Activity {
+    static String TAG = "Look at this"; //you can add this to error catch statements to check in logcat
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+
+    }
+}
